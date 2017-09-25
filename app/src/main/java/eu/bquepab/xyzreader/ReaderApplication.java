9b166d9830e8fdfac15fff87ab3b0958073a94c1
Borja Quevedo <borjota@gmail.com>
2017-09-25 12:05:51 +0200

    Add Timber dependency

---
+++ app/src/main/java/eu/bquepab/xyzreader/ReaderApplication.java
@@ -0,0 +1,16 @@
+package eu.bquepab.xyzreader;
+
+import android.app.Application;
+import timber.log.Timber;
+
+public class ReaderApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}

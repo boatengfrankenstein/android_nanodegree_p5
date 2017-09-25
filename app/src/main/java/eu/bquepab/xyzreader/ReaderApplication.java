cd698b610adadf0db41e509fb43ca952a301965e
Borja Quevedo <borjota@gmail.com>
2017-09-25 17:06:40 +0200

    Add LeakCanary to detect memory leaks

--- app/src/main/java/eu/bquepab/xyzreader/ReaderApplication.java
+++ app/src/main/java/eu/bquepab/xyzreader/ReaderApplication.java
@@ -1,6 +1,7 @@
 package eu.bquepab.xyzreader;
 
 import android.app.Application;
+import com.squareup.leakcanary.LeakCanary;
 import timber.log.Timber;
 
 public class ReaderApplication extends Application {
@@ -8,6 +9,14 @@ public class ReaderApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());

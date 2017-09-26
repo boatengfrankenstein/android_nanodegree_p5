16ae5fa51391bee97d32df7a5e6b56bb8dcf3683
Borja Quevedo <borjota@gmail.com>
2017-09-26 16:51:22 +0200

    Hide title in activity

--- app/src/main/java/eu/bquepab/xyzreader/ui/ArticleListActivity.java
+++ app/src/main/java/eu/bquepab/xyzreader/ui/ArticleListActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -63,6 +64,10 @@ public class ArticleListActivity extends AppCompatActivity
         swipeRefreshLayout.setOnRefreshListener(this);
 
         setSupportActionBar(toolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+        if (null != supportActionBar) {
+            supportActionBar.setDisplayShowTitleEnabled(false);
+        }
 
         getSupportLoaderManager().initLoader(0, null, this);
 

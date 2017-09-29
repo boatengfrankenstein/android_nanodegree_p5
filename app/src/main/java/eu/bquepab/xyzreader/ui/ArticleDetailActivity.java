bb34cc511c2e3a1e741760ce18368e557bbaa200
Borja Quevedo <borjota@gmail.com>
2017-09-29 10:20:34 +0200

    Included parallax effect in the Pager

--- app/src/main/java/eu/bquepab/xyzreader/ui/ArticleDetailActivity.java
+++ app/src/main/java/eu/bquepab/xyzreader/ui/ArticleDetailActivity.java
@@ -13,17 +13,20 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
+import butterknife.BindColor;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import eu.bquepab.xyzreader.R;
 import eu.bquepab.xyzreader.data.ArticleLoader;
 import eu.bquepab.xyzreader.data.ItemsContract;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * An activity representing a single Article detail screen, letting you swipe between articles.
  */
-public class ArticleDetailActivity extends AppCompatActivity
-        implements LoaderManager.LoaderCallbacks<Cursor> {
+public class ArticleDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private Cursor cursor;
     private long startId;
@@ -32,13 +35,15 @@ public class ArticleDetailActivity extends AppCompatActivity
     ViewPager pager;
     private CustomPagerAdapter pagerAdapter;
 
+    @BindColor(R.color.page_margin_drawable)
+    int colorPageMarginDrawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().getDecorView()
+                       .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         }
         setContentView(R.layout.activity_article_detail);
 
@@ -49,9 +54,9 @@ public class ArticleDetailActivity extends AppCompatActivity
         pagerAdapter = new CustomPagerAdapter(getSupportFragmentManager());
 
         pager.setAdapter(pagerAdapter);
-        pager.setPageMargin((int) TypedValue
-                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
-        pager.setPageMarginDrawable(new ColorDrawable(0x22000000));
+        pager.setPageMargin((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
+        pager.setPageMarginDrawable(new ColorDrawable(colorPageMarginDrawable));
+        pager.setPageTransformer(true, new ParallaxPageTransformer());
 
         if (savedInstanceState == null) {
             if (getIntent() != null && getIntent().getData() != null) {
@@ -106,6 +111,49 @@ public class ArticleDetailActivity extends AppCompatActivity
         @Override
         public int getCount() {
             return (cursor != null) ? cursor.getCount() : 0;
+        }
+    }
+
+    /**
+     * Base solution from:
+     * https://medium.com/@BashaChris/the-android-viewpager-has-become-a-fairly-popular-component-among-android-apps-its-simple-6bca403b16d4
+     */
+    public class ParallaxPageTransformer implements ViewPager.PageTransformer {
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(1);
+            } else if (position <= 1) { // [-1,1]
+                List<View> views = getAllViews(view);
+                for (int i = 0; i < views.size(); i++) {
+                    View child = views.get(i);
+                    if (child.getId() == R.id.photo) {
+                        child.setTranslationX(-position * (pageWidth / 2)); //Half the normal speed
+                        break;
+                    }
+                }
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(1);
+            }
+        }
+
+        private List<View> getAllViews(View v) {
+            if (!(v instanceof ViewGroup) || ((ViewGroup) v).getChildCount() == 0) { // It's a leaf
+                List<View> r = new ArrayList<>();
+                r.add(v);
+                return r;
+            } else {
+                List<View> list = new ArrayList<>();
+                list.add(v); // If it's an internal node add itself
+                int children = ((ViewGroup) v).getChildCount();
+                for (int i = 0; i < children; ++i) {
+                    list.addAll(getAllViews(((ViewGroup) v).getChildAt(i)));
+                }
+                return list;
+            }
         }
     }
 }
